ec1e54cd4d0eada74018aeb56124c06b3943f756
Gonzalo Donoso <gdonoso94@hotmail.com>
2021-01-24 17:35:20 +0100

    Refactored all paths

--- app/library/helpers.py
+++ app/library/helpers.py
@@ -4,9 +4,21 @@ from pathlib import Path
 import markdown
 from starlette.templating import Jinja2Templates
 
+def get_abs_path():
+    current_file = Path(__file__)
+    current_file_dir = current_file.parent
+    project_root = current_file_dir.parent
+    project_root_absolute = project_root.resolve()
+
+    return project_root_absolute
+
 
 def open_file(filename):
-    filepath = os.path.join("app/pages/", filename)
+    if not Path(filename).is_absolute():
+        filepath = os.path.join(get_abs_path(), f"pages/{filename}")
+    else:
+        filepath = filename
+
     with open(filepath, "r", encoding="utf-8") as input:
         page = input.read()
 
@@ -20,7 +32,8 @@ def markdown_to_html(filename: str):
 
 
 def get_blog_posts():
-    posts = {format_title(post): markdown.markdown(open_file(f"blog/{post}")) for post in os.listdir("app/pages/blog")
+    posts = {format_title(post): markdown.markdown(open_file(f"blog/{post}")) for post in
+             os.listdir(os.path.join(get_abs_path(), "pages/blog"))
              if
              post.endswith(".md")}
 
@@ -31,15 +44,6 @@ def format_title(title: str):
     date_and_title = datetime.strftime(datetime.strptime(title.split("_")[0], "%Y%M%d"), "%d/%M/%Y") + "|" + title
 
     return date_and_title
-
-
-def get_abs_path():
-    current_file = Path(__file__)
-    current_file_dir = current_file.parent
-    project_root = current_file_dir.parent
-    project_root_absolute = project_root.resolve()
-
-    return project_root_absolute
 
 
 def get_template(templates: Jinja2Templates, template_name: str):
--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ from app.library.helpers import *
 
 PROJECT_ROOT = get_abs_path().parent
 TEMPLATES_ROOT = os.path.join(PROJECT_ROOT, "templates")
+PAGES_ROOT = os.path.join(get_abs_path(), "pages")
+BLOG_ROOT = os.path.join(PAGES_ROOT, "blog")
 
 app = FastAPI()
 
@@ -19,6 +21,8 @@ app.mount("/resume", StaticFiles(directory=os.path.join(PROJECT_ROOT, "static/re
 app.mount("/home", StaticFiles(directory=os.path.join(PROJECT_ROOT, "static/home")), name="home")
 # app.mount("/home", StaticFiles(directory="static/home"), name="home")
 app.mount("/blog", StaticFiles(directory=os.path.join(PROJECT_ROOT, "static/blog")), name="blog")
+
+
 # app.mount("/blog", StaticFiles(directory="static/blog"), name="blog")
 
 
@@ -30,14 +34,14 @@ async def home(request: Request):
 @app.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
     data = {
-        "data": markdown_to_html("about_me.md")
+        "data": markdown_to_html(os.path.join(PAGES_ROOT, "about_me.md"))
     }
-    return templates.TemplateResponse("about.html", {"request": request, "data": data})
+    return templates.TemplateResponse(get_template(templates, "about.html"), {"request": request, "data": data})
 
 
 @app.get("/cv", response_class=HTMLResponse)
 async def cv(request: Request):
-    return templates.TemplateResponse("/cv/me.html", {"request": request})
+    return templates.TemplateResponse(get_template(templates, "/cv/me.html"), {"request": request})
 
 
 @app.get("/personal", response_class=HTMLResponse)
@@ -47,15 +51,16 @@ async def personal():
 
 @app.get("/professional", response_class=HTMLResponse)
 async def professional(request: Request):
-    return templates.TemplateResponse("home_blog.html", {"request": request, "data": get_blog_posts()})
+    return templates.TemplateResponse(get_template(templates, "home_blog.html"),
+                                      {"request": request, "data": get_blog_posts()})
 
 
 @app.get("/blog_post/{blog_post}", response_class=HTMLResponse)
 async def render_post(request: Request, blog_post: str):
     data = {
-        "page": markdown_to_html(f"blog/{blog_post}.md")
+        "page": markdown_to_html(os.path.join(PAGES_ROOT, f"blog/{blog_post}.md"))
     }
-    return templates.TemplateResponse("render_blog.html", {"request": request, "data": data})
+    return templates.TemplateResponse(get_template(templates, "render_blog.html"), {"request": request, "data": data})
 
 
 if __name__ == "__main__":
