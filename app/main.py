3931a495b3251e35894e8278af395f14318452e5
Gonzalo Donoso <gdonoso94@hotmail.com>
2021-01-24 18:26:11 +0100

    Passing application as an string

--- app/main.py
+++ app/main.py
@@ -64,4 +64,4 @@ async def render_post(request: Request, blog_post: str):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=2)
+    uvicorn.run("app", host="0.0.0.0", port=8000, workers=2)
